de60c8586415f128c156dccb1d19af500928b2c0
Israel Junio <israeljunio.2@gmail.com>
2020-06-13 17:12:04 -0300

    Troca mensagem na home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App com teste"/>
+    <HelloWorld msg="Woooowwww"/>
   </div>
 </template>
 
